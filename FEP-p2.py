f9b269aa4609f34fadd3fd9d09a44740b48b938e
Eugênio Piveta Pozzobon <eugenio.pozzobon@acad.ufsm.br>
2024-03-01 23:05:26 -0300

    update fep

--- FEP-p2.py
+++ FEP-p2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 ## PWM Inverter
 
-print("\n\n\n FLYBACK CCM")
+print("\n\n\n PWM Inverter")
 VinPico = 800
 VoutRMS = 565.68
 Pout = 30000
@@ -42,4 +42,27 @@ print('Total fet comutation losses', Poff + Pon)
 total_losses = power_dissip_fet + Poff + Pon
 print("Total Losses 1 fet:", total_losses)
 print("Total Losses x6 fet:", 6*total_losses)
-print("Eficienty:", Pout/(6*total_losses+Pout))
+print("Eficienty:", Pout/(3*total_losses+Pout))
+
+# thermal project
+
+Rjc = 0.52 # °C/W
+
+# Selected heat sink: OMNI-UNI-27-50 (2 FET POR HEAT SINK) https://br.mouser.com/datasheet/2/433/omniKlip_Wakefield_Vette_Data_Sheet-1500651.pdf
+# Thermal Paste: https://www.thermaltake.com/tg-50-thermal-compound.html
+
+Rdc = 0.0035/(8.38*2.7) # °C/W
+Rhs_ca = 0.8 # °C/W
+
+Rja = (Rhs_ca+Rdc+Rjc) # °C/W
+
+Tjmax = 150*0.9
+Tamb = 80
+
+RjaMax = (Tjmax-Tamb)/(total_losses) # °C/W
+
+print('\nProjeto Térmico')
+print('Rja Máximo', RjaMax)
+print('Rdc Obtido',Rdc)
+print('Rja Obtido',Rja)
+
